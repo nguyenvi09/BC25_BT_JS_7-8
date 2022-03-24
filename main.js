9a22b45433c28fa71eb0c1a45902ed4c226817c5
Nguyen <Tan-V.Nguyen@aia.com>
2022-03-24 17:05:44 +0700

    complete 4

--- main.js
+++ main.js
@@ -57,3 +57,50 @@ $("#demSoDuong").onclick = function(){
 };
 
 // 3. Tìm số nhỏ nhất trong mảng
+$("#soNhoNhat").onclick = function(){
+    var min = 0;
+    for(i = 0; i <= mangSoNguyen.length; i++){
+        if(mangSoNguyen[i] < min){
+            min = mangSoNguyen[i];
+        };
+    };
+
+    $("#kqSoNhoNhat").innerHTML = min;
+};
+
+// 4. Tìm số dương nhỏ nhất trong mảng
+/**
+ * giả sử mangSoNguyen = [-1, 0, 3, 2, 1, 5]
+ * i = 1 -> 0 > 0 flase
+ * i = 2 -> 3 > 0 
+ * 
+ * tìm số nguyên dương trong mảng trước rồi gán cho min
+ */
+
+// hàm lấy số nguyên dương đầu tiên trong mảng 
+function laySND(){
+    for(i = 0; i <= mangSoNguyen.length; i++){
+        if(mangSoNguyen[i] > 0){
+            return minSoNguyenDuong = mangSoNguyen[i];
+        };
+    }; 
+};
+
+// hàm tìm số nguyên dương nhỏ nhất
+
+function timSNDNN(){
+    for(i = 0; i <= mangSoNguyen.length; i++){
+        if(mangSoNguyen[i] > 0 && mangSoNguyen[i] <= minSoNguyenDuong){
+            minSoNguyenDuong = mangSoNguyen[i];
+        };
+    };
+
+    return minSoNguyenDuong;
+};
+
+$("#soDuongNhoNhat").onclick = function(){
+    laySND();
+    timSNDNN();
+
+    $("#kqSoDuongNhoNhat").innerHTML = minSoNguyenDuong;
+};
